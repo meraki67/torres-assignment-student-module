29222f8a5f5302f251ccbb38dd05b3b5294a9c29
Dave <davekentorres@gmail.com>
2025-01-10 23:27:28 +0800

    Merge all branches

---
+++ src/student/student.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Student } from './student.entity';
+
+@Injectable()
+export class StudentService {
+  constructor(
+    @InjectRepository(Student)
+    private readonly studentRepository: Repository<Student>,
+  ) {}
+
+  async createStudent(studentData: Partial<Student>): Promise<Student> {
+    const student = this.studentRepository.create(studentData);
+    return await this.studentRepository.save(student);
+  }
+
+  async getAllStudents(): Promise<Student[]> {
+    return await this.studentRepository.find();
+  }
+  
+  async updateStudent(id: number, updateData: Partial<Student>): Promise<Student> {
+    await this.studentRepository.update(id, updateData);
+    return this.studentRepository.findOneBy({ id });
+  }
+
+  async deleteStudent(id: number): Promise<void> {
+    await this.studentRepository.delete(id);
+  }
+}
